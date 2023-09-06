7ff60912487a0bc644fcae8ff7cf59bf9b9be403
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2023-09-06 11:17:12 -0400

    feat: endpoint products challenge

--- clean-architecture/endpoint-products/src/infrastructure/api/express.ts
+++ clean-architecture/endpoint-products/src/infrastructure/api/express.ts
@@ -1,10 +1,16 @@
 import express, { Express } from 'express';
 import { Sequelize } from 'sequelize-typescript';
 import CustomerModel from '../customer/repository/sequelize/customer.model';
+import { customerRoute } from './routes/customer.route';
+import { productRouter } from './routes/product.router';
+import ProductModel from '../product/repository/sequelize/product.model';
 
 export const app: Express = express();
 
 app.use(express.json());
+
+app.use('/customer', customerRoute);
+app.use('/product', productRouter);
 
 export let sequelize: Sequelize;
 
@@ -14,8 +20,7 @@ async function setupDb() {
     storage: ':memory:',
     logging: false,
   });
-  sequelize.addModels([CustomerModel]);
+  sequelize.addModels([CustomerModel, ProductModel]);
   await sequelize.sync();
 }
-
 setupDb();
